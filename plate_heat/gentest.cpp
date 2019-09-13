e0206056f5604a0e0213c90fa94d9d94acc6393d
Graeme Bragg <g.bragg@ecs.soton.ac.uk>
2019-09-13 20:20:22 +0100

    Update generator and type to have instrumentation and to be more flexible.

--- plate_heat/gentest.cpp
+++ plate_heat/gentest.cpp
@@ -1,7 +1,7 @@
 /*============================================================================
  * Name        : gentest.cpp
  * Author      : Graeme Bragg
- * Version     : 0.1
+ * Version     : 0.3.56
  * Description : Generates an example heated plate that is solved with a
  *                 packet storm.
  *                 Tested with C++17 (-std=c++17).
@@ -33,7 +33,7 @@
 #include <stdio.h>
 #include "args/args.hxx"
 
-float minChgF = 0.01;
+uint32_t yMax, xMax, nodeCount;
 
 struct fixedNode {
     uint32_t x;
@@ -42,14 +42,111 @@ struct fixedNode {
 
 };
 
+
+void writeDev(uint32_t x, uint32_t y, std::vector<fixedNode>& fNodes,
+                std::string& minChg, std::ofstream& gFile, std::fstream& eFile,
+                bool dummy)
+{
+    bool isFixed = false;
+    std::string temp;
+    
+    for (auto f : fNodes)//Work out if this is a fixed node.
+    {
+        if((f.x == x) && (f.y == y)) {
+            isFixed = true;
+            temp = std::to_string(f.t);
+            break;
+        }
+    }
+
+    if(isFixed)
+    {
+        gFile << "      <DevI id=\"c_" << x << "_" << y; 
+        gFile << "\" type=\"fixedNode\">";
+        gFile << "<P>\"t\": " << temp << ", \"x\": " << x; 
+        gFile << ", \"y\": " << y;
+        gFile << "</P></DevI>" << std::endl;
+        
+        if(dummy && (x < (xMax-1) || y < (yMax-1)))
+        {
+            // write a dummy padding  device 
+            gFile << "      <DevI id=\"dummy_" << x << "_" << y; 
+            gFile << "\" type=\"cell\"/>";
+            gFile << std::endl;
+        }
+    } else {
+        gFile << "      <DevI id=\"c_" << x << "_" << y;
+        gFile << "\" type=\"cell\">";
+        gFile << "<P>\"x\": " << x << ", \"y\": " << y;
+        if(minChg != "")
+        {
+            gFile << ", \"minChg\": " << minChg;
+        }
+        gFile << "</P></DevI>";
+        gFile << std::endl;
+
+        if(y < yMax-1) //North connection
+        {
+            eFile << "      <EdgeI path=\"c_";
+            eFile << x << "_" << y << ":north_in-";
+            eFile << "c_" << x << "_" << (y + 1);
+            eFile << ":out\"/>";
+            eFile << std::endl;
+        }
+
+        if(x < xMax-1) //East connection
+        {
+            eFile << "      <EdgeI path=\"c_";
+            eFile << x << "_" << y << ":east_in-";
+            eFile << "c_" << (x + 1);
+            eFile << "_" << y << ":out\"/>";
+            eFile << std::endl;
+        }
+
+        if(y > 0)   //South connection
+        {
+            eFile << "      <EdgeI path=\"c_";
+            eFile << x << "_" << y << ":south_in-";
+            eFile << "c_" << x << "_" << (y - 1);
+            eFile << ":out\"/>";
+            eFile << std::endl;
+        }
+
+        if(x > 0)   //West connection
+        {
+            eFile << "      <EdgeI path=\"c_";
+            eFile << x << "_" << y << ":west_in-";
+            eFile << "c_" << (x - 1);
+            eFile << "_" << y << ":out\"/>";
+            eFile << std::endl;
+        }
+        
+        eFile << "      <EdgeI path=\":instr-c_";
+        eFile << x << "_" << y;
+        eFile << ":instr\"/>" << std::endl;
+        eFile << std::endl;
+    }
+
+    eFile << "      <EdgeI path=\":finished-c_";
+    eFile << x << "_" << y;
+    eFile << ":finished\"/>" << std::endl;
+    eFile << std::endl;
+}
+
+
 int main(int argc, const char * argv[]) {
-    uint32_t yMax, xMax, nodeCount;
-    uint32_t x, y;
+    uint32_t squares;
+    uint32_t x, y, xStart, yStart, threadMaxCount;
+    std::vector<fixedNode> fNodes;
+    
     std::string minChg;
+    float fixTmp;
+    int fixPat;
 
     std::string gType;
     std::string gTypeFile;
     std::string gIDstr;
+    std::string gAppend;
     std::string gInstance;
 
     std::ofstream gFile;
@@ -71,14 +168,19 @@ int main(int argc, const char * argv[]) {
     args::ValueFlag<int> xIn(optional, "int", "Set X Dimension, default=3", {'x'});
     args::ValueFlag<int> yIn(optional, "int", "Set Y Dimension, default=3", {'y'});
     args::ValueFlag<int> dIn(optional, "int", "Set Both Dimensions, default=3", {'d'});
-    args::ValueFlag<float> mCh(optional, "float", "Set the minimum change in temperature required to trigger a message", {'m'});
-    args::ValueFlag<std::string> typF(optional, "filename", "Filename of the Type file, default=plate_heat_type.xml", {'t'});
+    args::ValueFlag<float> mCh(optional, "float", "Set the minimum change in temperature required to trigger a message. Default = 0.5", {'m'});
+    args::ValueFlag<float> tFx(optional, "float", "Set the temperature of the fixed nodes. Default = 100", {'t'});
+    args::ValueFlag<int> fNo(optional, "int", "Fixed-node pattern. (0) 2 opposite corners, (1) 4 opposite corners. Default=0", {'f'});
+    args::ValueFlag<std::string> typF(optional, "filename", "Filename of the Type file, default=plate_heat_type.xml", {'x', "type"});
+    args::ValueFlag<std::string> oAppend(optional, "string", "String to append to the generated filename before .xml, default=\"\"", {'o'});
+    args::ValueFlag<int> sq(optional, "int", "Set whether devices are generated linearly (0) or in blocks (1 = thread-level, 2 = box-level), default = 0", {'s'});
+    args::ValueFlag<int> tMC(optional, "int", "Set the maximum number of threads (used for Supervisor Instrumentation Array sizing). Default = 49152", {"ThreadMaxCount"});
 
 
     try
     {
         parser.ParseCLI(argc, argv);
-        } catch (args::Help) {
+        } catch (args::Help&) {
             std::cout << parser;
             return -1;
         } catch (const args::ParseError& e) {
@@ -92,7 +194,8 @@ int main(int argc, const char * argv[]) {
     }
 
     //Set Dimensions
-    if(dIn) {
+    if(dIn) 
+    {
           xMax = yMax = args::get(dIn);
     } else {
          xMax = (xIn)? args::get(xIn) : XDEFAULT;
@@ -100,14 +203,32 @@ int main(int argc, const char * argv[]) {
     }
 
     //Set the minChg
-    if(mCh) {
-        minChg = args::get(mCh);
+    if(mCh) 
+    {
+        minChg = std::to_string(args::get(mCh));
     } else {
-        minChg = std::to_string(minChgF);
+        minChg = "";
+    }
+    
+    //Set the fixed temperature
+    if(tFx) 
+    {
+        fixTmp = args::get(tFx);
+    } else {
+        fixTmp = 100.0;
+    }
+    
+    //Set the fixed node pattern
+    if(fNo) 
+    {
+        fixPat = args::get(fNo);
+    } else {
+        fixPat = 0;
     }
 
     //Use an alternative typefile
-    if(typF) {
+    if(typF)
+    {
         std::string tmpPath = args::get(typF);
         std::ifstream f(tmpPath.c_str());
         if(f.good())
@@ -115,13 +236,54 @@ int main(int argc, const char * argv[]) {
             gTypeFile = tmpPath;
         }
     }
-
+    
+    if(oAppend)
+    {
+        gAppend = args::get(oAppend);
+    } else {
+        gAppend = "";
+    }
+    
+    
+    if(sq)
+    {
+        squares = args::get(sq);
+    } else {
+        squares = 0;
+    }
+    
+    if(squares==2 && (xMax != yMax))
+    {
+      std::cout << "Error: s2 can only be used when x=y" << std::endl;
+      return 1;
+    }
+    
+    if(squares==2 && (xMax > 6144))
+    {
+      std::cout << "Error: s2 can only be used when x<=6144" << std::endl;
+      return 1;
+    }
+    
+    //Set the thread Max Count
+    if(tMC) 
+    {
+        threadMaxCount = args::get(tMC);
+    } else {
+        threadMaxCount = 49152;
+    }
 
     nodeCount = xMax * yMax;
 
-    std::vector<fixedNode> fNodes = { {0, 0, 100.0},
-                                    {(xMax - 1), (yMax - 1), -100.0}
-                                  };
+    switch(fixPat){
+      case 1:   fNodes = { {0, 0, fixTmp}, 
+                           {0, (yMax - 1), -fixTmp},
+                           {(xMax - 1), 0, -fixTmp},
+                           {(xMax - 1), (yMax - 1), fixTmp}
+                         };
+        
+      case 0:
+      default:  fNodes = {{0, 0, fixTmp}, {(xMax - 1), (yMax - 1), -fixTmp}};
+    }
 
     
 
@@ -131,11 +293,11 @@ int main(int argc, const char * argv[]) {
     gIDstr = ssID.str();
 
     //Open the files we need
-    gFile.open(gIDstr+".xml");        //The generated Graph
+    gFile.open(gIDstr+gAppend+".xml");        //The generated Graph
 
     tFile.open(gTypeFile);    //Type file
 
-    eFile.open(gIDstr+"_edges.xml", std::fstream::in | std::fstream::out | std::fstream::trunc); //Scratch file for the edges
+    eFile.open(gIDstr+gAppend+"_edges.xml", std::fstream::in | std::fstream::out | std::fstream::trunc); //Scratch file for the edges
 
     gFile.precision(2);    //Set float precision to 2D.P.
 
@@ -149,41 +311,42 @@ int main(int argc, const char * argv[]) {
     unsigned hcMaxScale, hbIdxScale;
     if(nodeCount<50000) {
         std::cout << "\t<1000" << std::endl;
-        hbIdxScale = 1000;
+        hbIdxScale = 5000;
         hcMaxScale = 10;
     } else if(nodeCount<100000) {
         std::cout << "\t<100000" << std::endl;
-        hbIdxScale = 10000;
+        hbIdxScale = 5000;
         hcMaxScale = 10;
-	} else if(nodeCount<100000) {
+    } else if(nodeCount<100000) {
         std::cout << "\t<100000" << std::endl;
-        hbIdxScale = 10000;
-        hcMaxScale = 100;
+        hbIdxScale = 5000;
+        hcMaxScale = 10;
     } else if(nodeCount<700000) {
         std::cout << "\t<700000" << std::endl;
-        hbIdxScale = 10000;
-        hcMaxScale = 100;
+        hbIdxScale = 5000;
+        hcMaxScale = 10;
     } else if (nodeCount<1000000) {
         std::cout << "\t<1000000" << std::endl;
-        hbIdxScale = 100000; 
-        hcMaxScale = 1000;
+        hbIdxScale = 7000; 
+        hcMaxScale = 10;
     } else {
         std::cout << "\t>1000000" << std::endl;
-        hbIdxScale = 100000;
-        hcMaxScale = 10000;
+        hbIdxScale = 2000;
+        hcMaxScale = 10;
     }
 
     //Copy graph type into graph instance.
     std::cout << "<GraphType id=\"" << gType << "\" >" << std::endl;
     while(std::getline(tFile, line))
     {
-        std::size_t XSIZE_pos, YSIZE_pos, NODE_pos, HCMAX_pos, HBIDX_pos;
+        std::size_t XSIZE_pos, YSIZE_pos, NODE_pos, HCMAX_pos, HBIDX_pos, THREAD_pos;
 
         std::string XSIZE_str ("XSIZE_DEF");
         std::string YSIZE_str ("YSIZE_DEF");
         std::string NODE_str ("NODE_DEF");
         std::string HCMAX_str ("HCMAX_DEF");
         std::string HBIDX_str ("HBIDX_DEF");
+        std::string THREAD_str ("THREAD_DEF");
 
         XSIZE_pos = line.find(XSIZE_str);
         if(XSIZE_pos != std::string::npos){
@@ -214,6 +377,12 @@ int main(int argc, const char * argv[]) {
             // Line contains HBIDX_DEF - replace with 
             line.replace(HBIDX_pos, HBIDX_str.length(), std::to_string(hbIdxScale));
         }
+        
+        THREAD_pos = line.find(THREAD_str);
+        if (THREAD_pos != std::string::npos){
+            // Line contains THREAD_DEF - replace with 
+            line.replace(THREAD_pos, THREAD_str.length(), std::to_string(threadMaxCount));
+        }
 
         gFile << line << std::endl;        //TODO: modify to only read graph instance, error handling, etc.
     }
@@ -234,74 +403,143 @@ int main(int argc, const char * argv[]) {
     eFile << "    <EdgeInstances>" << std::endl;
 
     //Loop through all devices and write device instance + edge instance.
-    for(x=0; x<xMax; x++)
+    //for(x=0; x<xMax; x++)
+    if(squares == 2)
     {
-       for(y=0; y<yMax; y++)
-       {
-            bool isFixed = false;            
-
-            std::string temp;
-
-            for ( auto f : fNodes )    //Work out if this is a fixed node.
+      /* Generate devices in a specific order so that they reduce the number of 
+       * connections leaving each thread/core/mailbox/board/box when used with
+       * the naive bucket-filling placement.
+       *
+       * This can generate a LOT of padding/dummy devices 
+       *
+       * This is nasty due to goto, but hey, this generator is quick & dirty,
+       * so fits the ethos...
+       */   
+      unsigned xCoreStart = 0, yCoreStart = 0;
+      
+       
+      //Generate a system-full
+      for(unsigned xBox = 0; xBox < 2; xBox++)
+      {
+        for(unsigned yBox = 0; yBox < 3; yBox++)
+        {
+      
+          //Generate a box-full
+          for(unsigned xBoard = 0; xBoard < 3; xBoard++)
+          {
+            for(unsigned yBoard = 0; yBoard < 2; yBoard++)
             {
-                if((f.x == x) && (f.y == y)) {
-                    isFixed = true;
-                    temp = std::to_string(f.t);
-                    break;
+              
+              //Generate a board-full
+              for(unsigned xMbox = 0; xMbox < 4; xMbox++)
+              {
+                for(unsigned yMbox = 0; yMbox < 4; yMbox++)
+                {
+                  std::cout << "xMbox:" << xMbox << "\tyMbox:" << yMbox << std::endl;
+                  
+                  
+                  // Generate a Mailbox full
+                  for(unsigned yCore = 0; yCore < 2; yCore++)
+                  {
+                    xCoreStart = 256*xMbox + 1024*xBoard + 3072*xBox;
+                    for(unsigned xCore = 0; xCore < 2; xCore++)
+                    {
+                      std::cout << "\txCore:" << xCore << "\tyCore:" << yCore; 
+                      std::cout << "\t\txCoreStart:" << xCoreStart;
+                      std::cout << "\tyCoreStart:" << yCoreStart << std::endl;
+                      
+                      
+                      
+                      yCoreStart = 128*yCore + 256*yMbox + 1024*yBoard + 2048*yBox;
+                      if(yCoreStart >= yMax && xCoreStart >= xMax)
+                      {
+                          goto bailout;
+                      } 
+                      else if(yCoreStart < yMax && xCoreStart < xMax) // Don't generate a core that is just padding
+                      {
+                        
+                        // Generate a core-full, pad everything
+                        for(unsigned xThread = 0; xThread < 4; xThread++,xCoreStart+=32)
+                        { 
+                          yStart = yCoreStart;
+                          std::cout << "\t\txThread:" << xThread << "\txStart:" << xCoreStart << std::endl;
+                          for(unsigned yThread = 0; yThread < 4; yThread++,yStart+=32)
+                          {
+                            std::cout << "\t\t\tyThread:" << yThread << "\tyStart:" << yStart << std::endl;
+                        
+                            // Generate a thread-full, pad everything 
+                            for(x=xCoreStart; x<(xCoreStart+32); x++)
+                            {
+                              for(y=yStart; y<(yStart+32); y++)
+                              {                   
+                                if(x >= xMax || y >= yMax)
+                                {
+                                  // write a dummy padding  device 
+                                  gFile << "      <DevI id=\"dummy_" << x << "_" << y;
+                                  gFile << "\" type=\"cell\"/>";
+                                  gFile << std::endl;
+                                }
+                                else
+                                {
+                                  writeDev(x, y, fNodes, minChg, gFile, eFile, true);
+                                }
+                              }
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+      
+
+    } else if(squares == 1) {
+        /* Generate devices in thread-size chunks (with dummy devices).
+         */ 
+        for(xStart = 0; xStart < xMax; xStart+=32)
+        {    
+            for(yStart = 0; yStart < yMax; yStart+=32)
+            {
+                for(x=xStart; x<(xStart+32); x++)
+                {
+                    for(y=yStart; y<(yStart+32); y++)
+                    {
+                        if(x >= xMax && y >= yMax)
+                        {
+                            
+                        }                    
+                        else if(x >= xMax || y >= yMax)
+                        {
+                            // write a dummy padding  device 
+                            gFile << "      <DevI id=\"dummy_" << x << "_" << y;
+                            gFile << "\" type=\"cell\"/>";
+                            gFile << std::endl;
+                        }
+                        else
+                        {
+                            writeDev(x, y, fNodes, minChg, gFile, eFile, true);
+                        }
+                    }
                 }
             }
-
-            if(isFixed)
+        }
+    } else {
+        /* Generate devices in coordinate order */
+        for(x=0; x<xMax; x++)
+        {
+            for(y=0; y<yMax; y++)
             {
-                gFile << "      <DevI id=\"c_" << x << "_" << y << "\" type=\"fixedNode\">";
-                gFile << "<P>\"t\": " << temp << ", \"x\": " << x << ", \"y\": " << y;
-                gFile << "</P></DevI>" << std::endl;
-
-            } else {
-                gFile << "      <DevI id=\"c_" << x << "_" << y << "\" type=\"cell\">";
-                gFile << "<P>\"x\": " << x << ", \"y\": " << y << ", \"minChg\": " << minChg << "</P></DevI>";
-                gFile << std::endl;
-
-                if(y < yMax-1) //North connection
-                {
-                    eFile << "      <EdgeI path=\"c_" << x << "_" << y << ":north_in-";
-                    eFile << "c_" << x << "_" << (y + 1) << ":out\"/>";
-                    eFile << std::endl;
-                }
-
-                if(x < xMax-1) //East connection
-                {
-                    eFile << "      <EdgeI path=\"c_" << x << "_" << y << ":east_in-";
-                    eFile << "c_" << (x + 1) << "_" << y << ":out\"/>";
-                    eFile << std::endl;
-                }
-
-                if(y > 0)   //South connection
-                {
-                    eFile << "      <EdgeI path=\"c_" << x << "_" << y << ":south_in-";
-                    eFile << "c_" << x << "_" << (y - 1) << ":out\"/>";
-                    eFile << std::endl;
-                }
-
-                if(x > 0)   //West connection
-                {
-                    eFile << "      <EdgeI path=\"c_" << x << "_" << y << ":west_in-";
-                    eFile << "c_" << (x - 1) << "_" << y << ":out\"/>";
-                    eFile << std::endl;
-                }
+                writeDev(x, y, fNodes, minChg, gFile, eFile, false);
             }
-
-            //Super Done connection
-            //eFile << "      <EdgeI path=\"c_" << x << "_" << y << ":done-:done";
-            //eFile << "\" />" << std::endl;
-
-            //Super Finished connection
-            eFile << "      <EdgeI path=\":finished-c_" << x << "_" << y;
-            eFile << ":finished\"/>" << std::endl;
-            eFile << std::endl;
-       }
+        }
     }
-
+    
+bailout:
     gFile << "    </DeviceInstances>" << std::endl;
     std::cout << std::endl << "    </DeviceInstances>" << std::endl;
     eFile << "    </EdgeInstances>" << std::endl;
@@ -315,7 +553,7 @@ int main(int argc, const char * argv[]) {
     }
     eFile.close();
 
-    remove((gIDstr+"_edges.xml").c_str());    //Clean up the edges file
+    remove((gIDstr+gAppend+"_edges.xml").c_str());    //Clean up the edges file
 
 
     //Write all of the graph-closing foo.
